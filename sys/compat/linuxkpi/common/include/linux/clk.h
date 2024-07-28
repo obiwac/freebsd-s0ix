0aeef4d1cd50de3730bf708e5c4b36c769cb462d
Aymeric Wibo <obiwac@gmail.com>
2024-07-29 01:30:20 +0200

    (vc4) linuxkpi: Add `linux/clk.h`

---
+++ sys/compat/linuxkpi/common/include/linux/clk.h
@@ -0,0 +1,9 @@
+/*-
+ * SPDX-License-Identifier: BSD-2-Clause
+ * Copyright (c) 2024 Aymeric Wibo <obiwac@freebsd.org>
+ */
+
+#ifndef _LINUXKPI_LINUX_CLK_H
+#define	_LINUXKPI_LINUX_CLK_H
+
+#endif	/* _LINUXKPI_LINUX_CLK_H */
