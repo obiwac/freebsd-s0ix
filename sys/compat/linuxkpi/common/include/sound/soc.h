c1b02c399d4f1e758f54992b3b7c8d003a2a38f2
Aymeric Wibo <obiwac@gmail.com>
2024-07-29 01:30:56 +0200

    (vc4) linuxkpi: Add `sound/soc.h`

---
+++ sys/compat/linuxkpi/common/include/sound/soc.h
@@ -0,0 +1,18 @@
+/*-
+ * SPDX-License-Identifier: BSD-2-Clause
+ * Copyright (c) 2024 Aymeric Wibo <obiwac@freebsd.org>
+ */
+
+#ifndef _LINUXKPI_SOUND_SOC_H
+#define	_LINUXKPI_SOUND_SOC_H
+
+struct snd_soc_card {
+};
+
+struct snd_soc_dai_link {
+};
+
+struct snd_soc_dai_link_component {
+};
+
+#endif	/* _LINUXKPI_SOUND_SOC_H */
