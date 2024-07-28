552f6ad224936dde5f6a98f9d6d46a01a54bce80
Aymeric Wibo <obiwac@gmail.com>
2024-07-29 01:30:49 +0200

    (vc4) linuxkpi: Add `sound/dmaengine_pcm.h`

---
+++ sys/compat/linuxkpi/common/include/sound/dmaengine_pcm.h
@@ -0,0 +1,12 @@
+/*-
+ * SPDX-License-Identifier: BSD-2-Clause
+ * Copyright (c) 2024 Aymeric Wibo <obiwac@freebsd.org>
+ */
+
+#ifndef _LINUXKPI_SOUND_DMAENGINE_PCM_H
+#define	_LINUXKPI_SOUND_DMAENGINE_PCM_H
+
+struct snd_dmaengine_dai_dma_data {
+};
+
+#endif	/* _LINUXKPI_SOUND_DMAENGINE_PCM_H */
