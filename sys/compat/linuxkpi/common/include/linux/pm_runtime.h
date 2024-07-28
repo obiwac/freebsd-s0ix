9453be3b388174b773bdad8d7fb771405d7c4236
Aymeric Wibo <obiwac@gmail.com>
2024-07-28 21:09:36 +0200

    (vc4) linuxkpi: `pm_runtime_resume_and_get`

--- sys/compat/linuxkpi/common/include/linux/pm_runtime.h
+++ sys/compat/linuxkpi/common/include/linux/pm_runtime.h
@@ -46,4 +46,11 @@ pm_runtime_suspended(struct device *dev)
 	return 0;
 }
 
+static inline int
+pm_runtime_resume_and_get(struct device *dev)
+{
+	pr_debug("%s: TODO\n", __func__);
+	return 0;
+}
+
 #endif	/* _LINUXKPI_LINUX_PM_RUNTIME_H_ */
