d11d61f9c597d08d836e9320427141555b5eeb0b
Aymeric Wibo <obiwac@gmail.com>
2024-07-29 01:30:31 +0200

    (vc4) linuxkpi: Add `media/cec.h`

---
+++ sys/compat/linuxkpi/common/include/media/cec.h
@@ -0,0 +1,12 @@
+/*-
+ * SPDX-License-Identifier: BSD-2-Clause
+ * Copyright (c) 2024 Aymeric Wibo <obiwac@freebsd.org>
+ */
+
+#ifndef _LINUXKPI_MEDIA_CEC_H
+#define	_LINUXKPI_MEDIA_CEC_H
+
+struct cec_msg {
+};
+
+#endif	/* _LINUXKPI_MEDIA_CEC_H */
