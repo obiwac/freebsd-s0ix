9d5d5189202cb6e5d5e1b73c0a6b8268a5b5881e
Aymeric Wibo <obiwac@gmail.com>
2024-07-29 16:52:20 +0200

    (vc4) linuxkpi: Bunch of stubs in `linux/clk.h`

--- sys/compat/linuxkpi/common/include/linux/clk.h
+++ sys/compat/linuxkpi/common/include/linux/clk.h
@@ -6,4 +6,71 @@
 #ifndef _LINUXKPI_LINUX_CLK_H
 #define	_LINUXKPI_LINUX_CLK_H
 
+#include <linux/device.h>
+
+struct clk {
+};
+
+static inline void
+clk_enable(struct clk *clk)
+{
+
+	pr_debug("%s: TODO\n", __func__);
+}
+
+static inline void
+clk_disable(struct clk *clk)
+{
+
+	pr_debug("%s: TODO\n", __func__);
+}
+
+static inline void
+clk_prepare(struct clk *clk)
+{
+
+	pr_debug("%s: TODO\n", __func__);
+}
+
+static inline void
+clk_unprepare(struct clk *clk)
+{
+
+	pr_debug("%s: TODO\n", __func__);
+}
+
+static inline void
+clk_disable_unprepare(struct clk *clk)
+{
+
+	clk_disable(clk);
+	clk_unprepare(clk);
+}
+
+static inline int
+clk_prepare_enable(struct clk *clk)
+{
+
+	clk_prepare(clk);
+	clk_enable(clk);
+
+	return (0);
+}
+
+static inline int
+clk_set_rate(struct clk *clk, uint32_t rate)
+{
+	
+	pr_debug("%s: TODO\n", __func__);
+	return (0);
+}
+
+static inline struct clk *
+devm_clk_get(struct device *dev, char const *id)
+{
+
+	pr_debug("%s: TODO\n", __func__);
+	return (NULL);
+}
+
 #endif	/* _LINUXKPI_LINUX_CLK_H */
