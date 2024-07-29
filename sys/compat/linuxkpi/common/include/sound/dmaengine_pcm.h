616615d4543ea03973fc10ee833fea3e6bc26792
Aymeric Wibo <obiwac@gmail.com>
2024-07-29 23:58:02 +0200

    (vc4) linuxkpi/sound/dmaengine_pcm: Stubs

--- sys/compat/linuxkpi/common/include/sound/dmaengine_pcm.h
+++ sys/compat/linuxkpi/common/include/sound/dmaengine_pcm.h
@@ -6,7 +6,25 @@
 #ifndef _LINUXKPI_SOUND_DMAENGINE_PCM_H
 #define	_LINUXKPI_SOUND_DMAENGINE_PCM_H
 
+#include <linux/dmaengine.h>
+#include <sound/soc.h>
+
 struct snd_dmaengine_dai_dma_data {
+	uint32_t	addr;
+	size_t	addr_width;
+	uint32_t	maxburst;
 };
+
+static inline int
+devm_snd_dmaengine_pcm_register(
+	struct device *dev,
+	struct snd_dmaengine_pcm_config const *config,
+	uint32_t flags
+)
+{
+
+	pr_debug("%s: TODO\n", __func__);
+	return (0);
+}
 
 #endif	/* _LINUXKPI_SOUND_DMAENGINE_PCM_H */
