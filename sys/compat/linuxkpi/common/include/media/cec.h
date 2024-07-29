34c8ce654c03c233aba681532ef1efaf512af6e7
Aymeric Wibo <obiwac@gmail.com>
2024-07-29 23:57:41 +0200

    (vc4) linuxkpi/media/cec: Stubs

--- sys/compat/linuxkpi/common/include/media/cec.h
+++ sys/compat/linuxkpi/common/include/media/cec.h
@@ -6,7 +6,28 @@
 #ifndef _LINUXKPI_MEDIA_CEC_H
 #define	_LINUXKPI_MEDIA_CEC_H
 
+#include <linux/kernel.h>
+
+#define	CEC_PHYS_ADDR_INVALID	0xFFFF
+
 struct cec_msg {
 };
+
+struct cec_adapter {
+};
+
+static inline void
+cec_s_phys_addr(struct cec_adapter *adapter, uint16_t phys_addr, bool block)
+{
+
+	pr_debug("%s: TODO\n", __func__);
+}
+
+static inline void
+cec_phys_addr_invalidate(struct cec_adapter *adapter)
+{
+
+	cec_s_phys_addr(adapter, CEC_PHYS_ADDR_INVALID, false);
+}
 
 #endif	/* _LINUXKPI_MEDIA_CEC_H */
